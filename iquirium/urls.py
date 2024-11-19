1a829ad153b8c752a7b0c17abfd80119fcfc5edc
Bernie2103 <142924229+Bernie2103@users.noreply.github.com>
2024-11-18 22:46:44 -0300

    Startup do projeto

---
+++ iquirium/urls.py
@@ -0,0 +1,26 @@
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.urls import path, include
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Iquirium API",
+        default_version='v1',
+        description="Documentação da API do Projeto Iquirium",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contato@exemplo.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('api/', include('app.user.urls')),
+    path('api/', include('app.member.urls')),
+    path('api/', include('app.product_feedback.urls')),
+    path('api/', include('app.swot.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
